3bb92c4cf010460331ef6be9cab66e3d8a45d88d
Son TRAN <son.tran.be@gmail.com>
2025-04-30 22:08:33 +0200

    Cleaning and training on shaders

--- exercise-2/main.cxx
+++ exercise-2/main.cxx
@@ -2,7 +2,9 @@
 #include "../libdearimgui/dearimgui/ImBackend/imgui_impl_glfw.h"
 #include "../libdearimgui/dearimgui/ImBackend/imgui_impl_opengl3.h"
 #include <stdio.h>
-#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+#include "../libdearimgui/dearimgui/glad/glad.h" /// Always load before GLFW
+#include <GLFW/glfw3.h>                          /// Will drag system OpenGL headers
 #include <GL/glu.h>
 
 #include <iostream>
@@ -48,23 +50,6 @@ unsigned int cubeIndices[] = {
     3, 2, 6,
     6, 7, 3
 };
-
-void drawCube(float angle)
-{
-    glPushMatrix();
-    glRotatef(angle, 1.0f, 1.0f, 0.0f);
-
-    glBegin(GL_TRIANGLES);
-    for (int i = 0; i < 36; ++i)
-    {
-        int idx = cubeIndices[i];
-        glVertex3f(cubeVertices[3 * idx], cubeVertices[3 * idx + 1], cubeVertices[3 * idx + 2]);
-    }
-    glEnd();
-
-    glPopMatrix();
-
-}
 
 // #################################################################################
 
@@ -118,58 +103,108 @@ int contextRendering(configData& configData)
     glViewport(0, 0, display_w, display_h);
     glClearColor(configData.mainv_clearColor.x * configData.mainv_clearColor.w, configData.mainv_clearColor.y * configData.mainv_clearColor.w, configData.mainv_clearColor.z * configData.mainv_clearColor.w, configData.mainv_clearColor.w);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    /// ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-
-
-
-    // Draw cube
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(45.0f, (float)display_w / (float)display_h, 0.1f, 100.0f);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glTranslatef(0.0f, 0.0f, -3.0f);
-
-    configData.demo_cube_angle += 0.5f;
-    drawCube(configData.demo_cube_angle);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 
 
 
 
     ///
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    // ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
     return 0;
 };
+
+unsigned compileShader(unsigned shaderType, const std::string& shaderSource)
+{
+    unsigned shaderID = glCreateShader(shaderType);       /// Create shader
+    const char* shaderCharSource = shaderSource.c_str(); 
+    glShaderSource(shaderID, 1, &shaderCharSource, NULL); /// Load shader source code, assuming NULL as termination
+    glCompileShader(shaderID);                            /// Compile shader
+
+    /// Check shader compilation
+    int  successCode;
+    glGetShaderiv(shaderID, GL_COMPILE_STATUS, &successCode);
+    /// Check success code
+    if(!successCode)
+    {
+        /// Get log length, and create a char array of length
+        int length;
+        glGetShaderiv(shaderID,GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*) alloca(length*sizeof(char)); /// alloc on stack
+        glGetShaderInfoLog(shaderID, length, NULL, message);
+        std::cout << "Failed to compile shader \n" << message << std::endl;
+        glDeleteShader(shaderID);
+        return 0;
+    }
+    return shaderID;
+}
+
+unsigned createShaderProgram(const std::string& vertexShaderSource, const std::string& fragmentShaderSource)
+{
+    unsigned shaderProgramID = glCreateProgram();
+    unsigned vertexShader   = compileShader(GL_VERTEX_SHADER,vertexShaderSource);
+    unsigned fragmentShader = compileShader(GL_FRAGMENT_SHADER,fragmentShaderSource);
+    
+    /// Attach shader buffer to shader program
+    glAttachShader(shaderProgramID, vertexShader);
+    glAttachShader(shaderProgramID, fragmentShader);
+    /// Link all contained shader
+    glLinkProgram(shaderProgramID);
+    // Check for linking errors
+    int  successCode;
+    glGetProgramiv(shaderProgramID, GL_LINK_STATUS, &successCode);
+    if (!successCode) {
+        /// Get log length, and create a char array of length
+        int length;
+        glGetShaderiv(shaderProgramID,GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*) alloca(length*sizeof(char)); /// alloc on stack
+        glGetProgramInfoLog(shaderProgramID, length, NULL, message);
+        std::cout << "Failed to link the shader\n" << message << std::endl;
+        glDeleteProgram(shaderProgramID);
+        return 0;
+    }
+    // Cleaning up shader
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    return shaderProgramID;
+}
 
 // #################################################################################
 
 // Main code
 int main(int, char**)
 {
-    glfwSetErrorCallback(glfw_error_callback);
-    if (!glfwInit())
-        return 1;
-
-    // GL 3.0 + GLSL 130
-    const char* glsl_version = "#version 130";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
-
     /// Initialize configuration
     configData configData;
-    configData.glfwWindow = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
 
-    // Create window with graphics context
-    if (configData.glfwWindow  == nullptr)
-        return 1;
+
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit()) return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /// Create window
+    configData.glfwWindow = glfwCreateWindow(1280, 720, "sntengine3D", nullptr, nullptr);
+    if(configData.glfwWindow == nullptr)
+    {
+        glfwTerminate();
+        return -1;    
+    }
+    /// Setup glfw context
     glfwMakeContextCurrent(configData.glfwWindow);
     glfwSwapInterval(1); // Enable vsync
+
+    /// Initialize GLAD
+    if( !gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        return -1;
+    }
+
 
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
@@ -199,6 +234,102 @@ int main(int, char**)
     ImGui_ImplOpenGL3_Init(glsl_version);
 
     glEnable(GL_DEPTH_TEST);
+
+
+    /// OpenGL initialization context
+    const char *vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+    // unsigned int vertexShader;                                   /// Init shader ID
+    // vertexShader = glCreateShader(GL_VERTEX_SHADER);             /// Create vertex shader
+    // glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);  /// Load shader source code
+    // glCompileShader(vertexShader);                               /// Compile shader
+
+    // /// Check shader compilation
+    // int  success;
+    // char infoLog[512];
+    // glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    // if(!success)
+    // {
+    //     glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    //     std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    //     return -1;
+    // }
+
+    const char *fragmentShaderSource ="#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\0";
+
+    // unsigned int fragmentShader;
+    // fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    // glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    // glCompileShader(fragmentShader);
+
+    // unsigned shaderProgram;
+    // shaderProgram = glCreateProgram();
+    // glAttachShader(shaderProgram, vertexShader);
+    // glAttachShader(shaderProgram, fragmentShader);
+    // glLinkProgram(shaderProgram);
+    // // check for linking errors
+    // glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    // if (!success) {
+    //     glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    //     std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    // }
+    // glDeleteShader(vertexShader);
+    // glDeleteShader(fragmentShader);
+
+    unsigned shaderProgram = createShaderProgram(vertexShaderSource,fragmentShaderSource);
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0); 
+
     // Main loop
     while (!glfwWindowShouldClose(configData.glfwWindow))
     {
@@ -215,6 +346,13 @@ int main(int, char**)
 
         /// main viewport rendering
         contextRendering(configData);
+
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // glBindVertexArray(0); // no need to unbind it every time 
 
         // Update and Render additional Platform Windows
         if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
